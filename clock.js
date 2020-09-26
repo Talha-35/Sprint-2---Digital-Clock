36b17507dbb9385d9a2646d7d4e5277188f6f48f
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-09-26 22:07:48 +0200

    JS file is updated

--- clock.js
+++ clock.js
@@ -9,6 +9,15 @@ function clock() {
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
 
+    if (h < 10){
+        h = `0${h}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
+    if (s < 10) {
+        s = `0${s}`;
+    }
   
     hour.innerHTML = h;
     minutes.innerHTML = m;
